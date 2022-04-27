aecd1b8a702cac49747c5a5bb0921a4d5c975f5c
Aurélien GARNIER <au2001sfg@gmail.com>
2022-04-27 03:33:11 +0200

    Add SASL authentication

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,16 @@ FROM alpine:latest
 LABEL maintainer="Aurélien GARNIER <me@arl.sh>"
 
 RUN apk update
-RUN apk add --upgrade supervisor postfix
+RUN apk add --upgrade supervisor postfix cyrus-sasl cyrus-sasl-login
 RUN rm -rf /etc/apk/cache
 
-COPY ./supervisord.conf /etc/supervisor/conf.d/
+RUN mkdir -p /etc/sasl2
 
-COPY ./postfix.sh /
-RUN chmod +x /postfix.sh
+COPY ./supervisord.conf /etc/supervisor/conf.d/
+COPY ./sasl.conf /etc/postfix/sasl/smtpd.conf
 
 COPY ./entrypoint.sh /
 RUN chmod +x /entrypoint.sh
-
 ENTRYPOINT ["/entrypoint.sh"]
 
 CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,23 @@
 
 MAIL_DOMAIN=${MAIL_DOMAIN:=example.com}
 MAIL_HOST=${MAIL_HOST:-$MAIL_DOMAIN}
+SMTP_USERNAME=${SMTP_USERNAME:=user}
+SMTP_PASSWORD=${SMTP_PASSWORD:=Password123}
 
 # Postfix
 
 postconf -e myhostname=${MAIL_HOST}
-postconf -e mydomain=${MAIL_DOMAIN}
 postconf -e myorigin=${MAIL_DOMAIN}
 postconf -e maillog_file=/var/log/mail.log
+
+# SASL
+
+postconf -e smtpd_sasl_auth_enable=yes
+postconf -e broken_sasl_auth_clients=yes
+postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
+
+echo "${SMTP_PASSWORD}" | saslpasswd2 -pcu "${MAIL_HOST}" "${SMTP_USERNAME}"
+chown postfix /etc/sasl2/sasldb2
 
 # TLS
 
@@ -19,6 +29,7 @@ KEY_FILE=/etc/postfix/certs/${MAIL_HOST}.key
 
 if [[ -f "${CRT_FILE}" && -f "${KEY_FILE}" ]]; then
   postconf -e smtpd_use_tls=yes
+  postconf -e smtpd_enforce_tls=yes
   postconf -e smtpd_tls_cert_file=${CRT_FILE}
   postconf -e smtpd_tls_key_file=${KEY_FILE}
   postconf -e smtpd_tls_security_level=may
