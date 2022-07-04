eb688390eb8fcbb49cd8fef9cca17ff651de480f
Aurélien <au2001sfg@gmail.com>
2022-07-04 20:48:16 +0200

    Fix policyd-spf socket path

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,8 @@ postconf -e myorigin="${MAIL_DOMAIN}"
 postconf -e mydestination=""
 postconf -e virtual_mailbox_domains="${MAIL_DOMAIN}"
 
-# postconf -e smtpd_recipient_restrictions="permit_mynetworks, permit_sasl_authenticated, reject_unknown_client_hostname, reject_unauth_destination, check_policy_service unix:policyd-spf/policyd-spf"
-postconf -e smtpd_recipient_restrictions="permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination, check_policy_service unix:policyd-spf/policyd-spf, permit"
+# postconf -e smtpd_recipient_restrictions="permit_mynetworks, permit_sasl_authenticated, reject_unknown_client_hostname, reject_unauth_destination, check_policy_service unix:private/policyd-spf"
+postconf -e smtpd_recipient_restrictions="permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination, check_policy_service unix:private/policyd-spf, permit"
 postconf -e smtpd_helo_required="yes"
 postconf -e smtpd_helo_restrictions="permit_mynetworks, permit_sasl_authenticated, reject_invalid_helo_hostname, reject_non_fqdn_helo_hostname, permit"
 
